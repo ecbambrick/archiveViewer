e25da09c8ebf2580a0f9bc3e11c335d573e6e96d
Cole Bambrick <kcirbmab@gmail.com>
2014-07-12 18:16:23 -0400

    Updated Imaging::halfScale to support being cancelled if running in a separate thread.
    Renamed to halfScaled and added a QFutureWatcher<void>* argument. Also,
    added comments and did minor refactoring.

--- src/imaging.cpp
+++ src/imaging.cpp
@@ -7,31 +7,40 @@ quint32 pixelAverage(quint32 a, quint32 b)
 }
 
 // From https://blog.qt.digia.com/blog/2009/01/20/50-scaling-of-argb32-image/
-QImage Imaging::halfScale(const QImage &source)
+QImage Imaging::halfScaled(const QImage &source, QFutureWatcher<void> *watcher)
 {
     if (source.width() % 2 != 0 || source.height() % 2 != 0) {
         throw std::invalid_argument("Image must have even dimensions");
     }
 
-    QImage dest(source.size() * 0.5, QImage::Format_ARGB32_Premultiplied);
-
+    // Get the source and destination images' data as unsigned integers.
+    QImage destination(source.size() * 0.5, QImage::Format_ARGB32_Premultiplied);
     const quint32 *src = reinterpret_cast<const quint32*>(source.bits());
+    quint32 *dst = reinterpret_cast<quint32*>(destination.bits());
+
+    // Get the number of bytes to skip each iteration of the y-value.
+    int dx = destination.bytesPerLine() >> 2;
     int sx = source.bytesPerLine() >> 2;
     int sx2 = sx << 1;
 
-    quint32 *dst = reinterpret_cast<quint32*>(dest.bits());
-    int dx = dest.bytesPerLine() >> 2;
-    int ww = dest.width();
-    int hh = dest.height();
+    // Scale.
+    for (int y = destination.height(); y; --y, dst += dx, src += sx2) {
 
-    for (int y = hh; y; --y, dst += dx, src += sx2) {
+        // Cancel the scaling if the watcher has been cancelled.
+        if (watcher != nullptr && watcher->isCanceled()) {
+            break;
+        }
+
+        // Get the pixel data for the current row.
         const quint32 *p1 = src;
         const quint32 *p2 = src + sx;
         quint32 *q = dst;
-        for (int x = ww; x; --x, q++, p1 += 2, p2 += 2)
-            * q = pixelAverage(pixelAverage(p1[0], p1[1]),
-                               pixelAverage(p2[0], p2[1]));
+
+        // Update the pixel data for the current row.
+        for (int x = destination.width(); x; --x, q++, p1 += 2, p2 += 2) {
+            * q = pixelAverage(pixelAverage(p1[0], p1[1]), pixelAverage(p2[0], p2[1]));
+        }
     }
 
-    return dest;
+    return destination;
 }
--- src/imaging.h
+++ src/imaging.h
@@ -1,17 +1,21 @@
 #ifndef IMAGING_H
 #define IMAGING_H
 #include <QImage>
+#include <QFutureWatcher>
 
 namespace Imaging
 {
     ///
     /// \brief Scales a copy of the given image to half its size and returns
     /// that copy. This function has similar results to using smooth scaling,
-    /// but is much faster.
-    /// \param image The image to scale.
+    /// but is much faster. If the function is called concurrently, an optional
+    /// watcher parameter can be passed to determine wither or not the operation
+    /// should be cancelled.
+    /// \param source The image to scale.
+    /// \param watcher The future watcher that calls the function.
     /// \return A copy of the image, scaled to half size.
     ///
-    QImage halfScale(const QImage &image);
+    QImage halfScaled(const QImage &source, QFutureWatcher<void> *watcher = nullptr);
 }
 
 #endif // IMAGING_H
