35015edf0caae48c9bd6a4b66af37cff48e4d1bd
Cole Bambrick <kcirbmab@gmail.com>
2014-07-13 08:19:09 -0400

    Added the Imaging::bilinearScale() function
    Scales a copy of the given image using bilinear resampling and returns
    that copy. This function should generally only be used when scaling less
    than 50%. Anything beyond this bound will have similar quality to
    nearest neighbor.

--- src/imaging.cpp
+++ src/imaging.cpp
@@ -6,6 +6,82 @@ quint32 pixelAverage(quint32 a, quint32 b)
     return ((((a ^ b) & 0xfefefefeUL) >> 1) + (a & b));
 }
 
+QImage Imaging::bilinearScaled(const QImage &source, const QSize &size, QFutureWatcher<void> *watcher)
+{
+    if (size.width() > source.width() || size.height() > source.height()) {
+        throw std::out_of_range("Size must be smaller than or equal to the source image.");
+    }
+
+    // Calculate the ratio of the source and destionation's dimensions.
+    float xRatio = (float)(source.width() - 1)/size.width();
+    float yRatio = (float)(source.height() - 1)/size.height();
+
+    // Get the source and destination images' data as unsigned integers.
+    QImage destination = QImage(size, QImage::Format_ARGB32_Premultiplied);
+    quint32 *dst = reinterpret_cast<quint32*>(destination.bits());
+    const quint32 *src = reinterpret_cast<const quint32*>(source.bits());
+
+    // Keep track of the pixel position of the destination image.
+    int offset = 0;
+
+    // Scale the image.
+    for (int i = 0; i < destination.height(); i++) {
+
+        // Cancel the scaling if the watcher has been cancelled.
+        if (watcher != nullptr && watcher->isCanceled()) {
+            break;
+        }
+
+        for (int j = 0; j < destination.width(); j++) {
+
+            // Calculate the (x,y) position of the next pixel to
+            float x = (int)(xRatio * j);
+            float y = (int)(yRatio * i);
+            int index = y * source.width() + x;
+
+            // Calculate the differences between the source and destination
+            // images at the current pixel.
+            float xDifference = (xRatio * j) - x ;
+            float yDifference = (yRatio * i) - y ;
+
+            // Get a 2x2 square of pixels at the calculated position.
+            int a = src[index];
+            int b = src[index+1];
+            int c = src[index+source.width()];
+            int d = src[index+source.width()+1];
+
+            // Calculate the blue element.
+            float blue = (a&0xff)*(1-xDifference)*(1-yDifference)
+                       + (b&0xff)*(xDifference)*(1-yDifference)
+                       + (c&0xff)*(yDifference)*(1-xDifference)
+                       + (d&0xff)*(xDifference*yDifference);
+
+            // Calculate the green element.
+            float green = ((a>>8)&0xff)*(1-xDifference)*(1-yDifference)
+                        + ((b>>8)&0xff)*(xDifference)*(1-yDifference)
+                        + ((c>>8)&0xff)*(yDifference)*(1-xDifference)
+                        + ((d>>8)&0xff)*(xDifference*yDifference);
+
+            // Calculate the red element.
+            float red = ((a>>16)&0xff)*(1-xDifference)*(1-yDifference)
+                      + ((b>>16)&0xff)*(xDifference)*(1-yDifference)
+                      + ((c>>16)&0xff)*(yDifference)*(1-xDifference)
+                      + ((d>>16)&0xff)*(xDifference*yDifference);
+
+            // Use a hard-coded value for the alpha element.
+            int alpha = 0xff000000;
+
+            // Calculate the pixel value for the destination image
+            dst[offset++] = alpha
+                          | ((((int)red)<<16)&0xff0000)
+                          | ((((int)green)<<8)&0xff00)
+                          | ((int)blue) ;
+        }
+    }
+
+    return destination;
+}
+
 // From https://blog.qt.digia.com/blog/2009/01/20/50-scaling-of-argb32-image/
 QImage Imaging::halfScaled(const QImage &source, QFutureWatcher<void> *watcher)
 {
--- src/imaging.h
+++ src/imaging.h
@@ -1,6 +1,7 @@
 #ifndef IMAGING_H
 #define IMAGING_H
 #include <QImage>
+#include <QSize>
 #include <QFutureWatcher>
 
 namespace Imaging
@@ -16,6 +17,17 @@ namespace Imaging
     /// \return A copy of the image, scaled to half size.
     ///
     QImage halfScaled(const QImage &source, QFutureWatcher<void> *watcher = nullptr);
+
+    /// \brief Scales a copy of the given image using bilinear resampling and
+    /// returns that copy. This function should generally only be used when
+    /// scaling less than 50%. Anything beyond this bound will have similar
+    /// quality to nearest neighbor.
+    /// \param source The image to scale.
+    /// \param size The size to scale the image to.
+    /// \param watcher The future watcher that calls the function.
+    /// \return A copy of the image, scaled to the given size.
+    ///
+    QImage bilinearScaled(const QImage &source, const QSize &size, QFutureWatcher<void> *watcher = nullptr);
 }
 
 #endif // IMAGING_H
