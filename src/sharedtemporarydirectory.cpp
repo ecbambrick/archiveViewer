4d315ed9989204a93adf89a3accf1be00cfd799f
Cole Bambrick <kcirbmab@gmail.com>
2014-11-16 11:09:30 -0500

    Minor refactoring of SharedTemporaryDirectory class

--- src/sharedtemporarydirectory.cpp
+++ src/sharedtemporarydirectory.cpp
@@ -18,32 +18,32 @@
 
 *******************************************************************************/
 
-#include "sharedtemporarydirectory.h"
 #include <QDateTime>
 #include <QDir>
+#include "sharedtemporarydirectory.h"
 
 // The file type extension of a temporary file.
 const auto fileType = ".archiveTemp";
 
-// The number of milliseconds to wait before updating the temporary file's
-// file name.
-const auto interval = 5 * 60 * 1000;
+// The number of milliseconds to wait before updating the temporary file.
+const auto interval = int(5 * 60 * 1000);
 
 // ------------------------------------------------------------------- public //
 
 SharedTemporaryDirectory::SharedTemporaryDirectory(const QString &path)
-    : _path(QDir::tempPath() + "/" + path)
-    , _id(tmpnam(NULL))
 {
-    if (path == NULL || path.isEmpty()) {
+    if (path.isNull() || path.isEmpty()) {
         throw std::invalid_argument("Temporary directory path cannot be empty");
     }
 
-    // Clean the ID by removing the trailing period and changing the slash.
-    _id.replace(0, 1, '/').chop(1);
+    _path = QDir::tempPath() + "/" + path;
+    _id = tmpnam(NULL);
 
-    // Create the temporary directory containing a temporary file.
+    // Create the temporary directory.
     QDir().mkpath(_path);
+
+    // Create a temporary file inside the temporary directory by opening it
+    // and closing it immediately.
     _temporaryFile = std::unique_ptr<QFile>(new QFile(_path + _id));
     _temporaryFile->open(QFile::WriteOnly);
     _temporaryFile->close();
@@ -60,21 +60,21 @@ SharedTemporaryDirectory::~SharedTemporaryDirectory()
 {
     auto directory = QDir(_path);
     auto currentTime = QDateTime::currentMSecsSinceEpoch();
-    auto tempFileTypes = QStringList(QString("*").append(fileType));
+    auto nameFilter = QStringList(QString("*").append(fileType));
 
     // Remove the temporary file and any other temporary files that have
     // expired. Expired files may be left behind by applications that have
     // unexpectedly quit before being able to clean up.
     _temporaryFile->remove();
-    for (QString fileName : directory.entryList(tempFileTypes)) {
-        auto timeStamp = fileName.section(".", 1, 1).toLongLong() + interval*2;
+    for (QString fileName : directory.entryList(nameFilter)) {
+        auto timeStamp = fileName.section(".", 1, 1).toLongLong() + interval * 2;
         if (timeStamp < currentTime) {
             QFile(_path + "/" + fileName).remove();
         }
     }
 
     // Only delete the temporary directory of no temporary files exist.
-    if (directory.entryList(tempFileTypes).length() <= 0) {
+    if (directory.entryList(nameFilter).isEmpty()) {
         directory.removeRecursively();
     }
 }
@@ -90,5 +90,5 @@ void SharedTemporaryDirectory::updateTemporaryFile()
 {
     auto timeStamp = QString::number(QDateTime::currentMSecsSinceEpoch());
 
-    _temporaryFile->rename(_path + _id + "." + timeStamp + fileType);
+    _temporaryFile->rename(_path + _id + timeStamp + fileType);
 }
